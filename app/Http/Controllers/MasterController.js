e6cae28fd4584edee55e08dcbc08b8603e68557d
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2023-04-26 18:15:01 +0700

    add more endpoint for master data

--- app/Http/Controllers/MasterController.js
+++ app/Http/Controllers/MasterController.js
@@ -1,4 +1,5 @@
 const axios = require('axios')
+const { or } = require('sequelize')
 const orderMicroservice = 'http://192.168.56.1:3000'
 
 const MasterController = {
@@ -41,6 +42,174 @@ const MasterController = {
                     status: "failed",
                     message: "gagal mengambil data",
                     data: err.message
+                })
+        })
+    },
+    getTaxInvoice: (req, res) => {
+        axios.get(`${orderMicroservice}/master/tax_invoice`, {
+            headers: {
+                "authorization": req.get('authorization')
+            }
+        }).then(result => {
+            res.status(200)
+                .json({
+                    status: "success",
+                    message: "berhasil mengambil data",
+                    data: result.data.data
+                })
+        }).catch(err => {
+            res.status(400)
+                .json({
+                    status: "failed",
+                    message: "gagal mengambil data",
+                    error: err.message
+                })
+        })
+    },
+    getAddrType: (req, res) => {
+        axios.get(`${orderMicroservice}/master/addr_type`, {
+            headers: {
+                "authorization": req.get("authorization")
+            }
+        }).then(result => {
+            res.status(200)
+                .json({
+                    status: "success",
+                    message: "berhasil mengambil data",
+                    data: result.data.data
+                })
+        }).catch(err => {
+            res.status(400)
+                .json({
+                    status: "failed",
+                    message: "gagal mengambil data",
+                    error: err.message
+                })
+        })
+    },
+    getContactPerson: (req, res) => {
+        axios.get(`${orderMicroservice}/master/contact_person`, {
+            headers: {
+                "authorization": req.get("authorization")
+            }
+        }).then(result => {
+            res.status(200)
+                .json({
+                    status: "success",
+                    message: "berhasil mengambil data",
+                    data: result.data.data
+                })
+        }).catch(err => {
+            res.status(400)
+                .json({
+                    status: "failed",
+                    message: "gagal mengmbil data",
+                    error: err.message
+                })
+        })
+    },
+    getBpType: (req, res) => {
+        axios.get(`${orderMicroservice}/master/bp_type`, {
+            headers: {
+                "authorization": req.get('authorization')
+            }
+        }).then(result => {
+            res.status(200)
+                .json({
+                    status: "success",
+                    message: "berhasil mengambil data",
+                    data: result.data.data
+                })
+        }).catch(err => {
+            res.status(400)
+                .json({
+                    status: "failed",
+                    message: "gagal mengambil data",
+                    error: err.message
+                })
+        })
+    },
+    getCitizen: (req, res) => {
+        axios.get(`${orderMicroservice}/master/citizen`, {
+            headers: {
+                "authorization": req.get('authorization')
+            }
+        }).then(result => {
+            res.status(200)
+                .json({
+                    status: "success",
+                    message: "berhasil mengambil data",
+                    data: result.data.data
+                })
+        }).catch(err => {
+            res.status(400)
+                .json({
+                    status: "failed",
+                    message: "gagal mengambil data",
+                    error: err.message
+                })
+        })
+    },
+    getBloodGroup: (req, res) => {
+        axios.get(`${orderMicroservice}/master/blood_group`, {
+            headers: {
+                "authorization": req.get('authorization')
+            }
+        }).then(result => {
+            res.status(200)
+                .json({
+                    status: "success",
+                    message: "berhasil mengambil data",
+                    data: result.data.data
+                })
+        }).catch(err => {
+            res.status(400)
+                .json({
+                    status: 'failed',
+                    message: 'gagal mengambil data',
+                    error: err.message
+                })
+        })
+    },
+    getGender: (req, res) => {
+        axios.get(`${orderMicroservice}/master/gender`, {
+            headers: {
+                "authorization": req.get('authorization')
+            }
+        }).then(result => {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    message: "berhasil mengambil data",
+                    data: result.data.data
+                })
+        }).catch(err => {
+            res.status(400)
+                .json({
+                    status: "failed",
+                    message: "gagal mengambil data",
+                    data: err.message
+                })
+        })
+    },
+    getCurrency: (req, res) => {
+        axios.get(`${orderMicroservice}/master/currency`, {
+            headers: {
+                'authorization': req.get('authorization')
+            }
+        }).then(result => {
+            res.status(200)
+                .json({
+                    status: "success",
+                    message: "berhasil mengambil data",
+                    data: result.data.data
+                })
+        }).catch(err => {
+            res.status(400)
+                .json({
+                    status: 'failed',
+                    message: "gagal mengmabil data",
+                    error: err.message
                 })
         })
     }
--- routes/master.js
+++ routes/master.js
@@ -4,10 +4,26 @@ const controller = require('../app/Http/Controllers/Controller')
 
 route = [
     '/periode', //0
-    '/periode-customer' //1
+    '/periode-customer', //1
+    '/tax_invoice', //2
+    '/addr_type', //3
+    '/contact_person', //4
+    '/bp_type', //5
+    '/citizen', //6
+    '/blood_group', //7
+    '/gender', //8
+    '/currency' //9
 ]
 
 router.get(route[0], controller.MasterController.getPeriode)
 router.get(route[1], controller.MasterController.getPeriodeSales)
+router.get(route[2], controller.MasterController.getTaxInvoice)
+router.get(route[3], controller.MasterController.getAddrType)
+router.get(route[4], controller.MasterController.getContactPerson)
+router.get(route[5], controller.MasterController.getBpType)
+router.get(route[6], controller.MasterController.getCitizen)
+router.get(route[7], controller.MasterController.getBloodGroup)
+router.get(route[8], controller.MasterController.getGender)
+router.get(route[9], controller.MasterController.getCurrency)
 
 module.exports = router
