a51e11b85cb89dc0db0936c57156f6882fc7c43d
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2023-04-28 11:10:13 +0700

    update feature to create partner and get detail customer

--- app/Http/Controllers/PartnerController.js
+++ app/Http/Controllers/PartnerController.js
@@ -45,6 +45,72 @@ const PartnerController = {
                     error: err.message
                 })
         })
+    },
+    createNewCustomer: (req, res) => {
+        axios.post(`${orderMicroservice}/partner/create-partner`, {
+            entityId: req.body.partnerEntity,
+            partnerName: req.body.partnerName,
+            partnerGroupId: req.body.partnerGroup,
+            partnerIsCustomer: req.body.partnerIsCustomer,
+            partnerIsVendor: req.body.partnerIsVendor,
+            partnerActive: req.body.partnerActive,
+            partnerCurrencyId: req.body.partnerCurrencyId,
+            partnerIsMember: req.body.partnerIsMember,
+            partnerIsEmployee: req.body.partnerIsEmployee,
+            partnerIsWriter: req.body.partnerIsWriter,
+            partnerTransactionCodeId: req.body.partnerTransactionCodeId,
+            partnerEmail: req.body.partnerEmail,
+            partnerNameAlternative: req.body.partnerNameAlternative,
+            partnerIsPs: req.body.partnerIsPs,
+            partnerIsBm: req.body.partnerIsBm,
+            partnerSex: req.body.partnerSex,
+            partnerBloodGroup: req.body.partnerBloodGroup,
+            partnerDateBirthday: req.body.partnerDateBirthday,
+            partnerNation: req.body.partnerNation,
+            partnerBpDate: req.body.partnerBpDate,
+            partnerBpType: req.body.partnerBpType,
+            partnerIsVolunteer: req.body.partnerIsVolunteer,
+            partnerIsSbm: req.body.partnerIsSbm,
+        },{
+            headers: {
+                "authorization": req.get('authorization')
+            }
+        }).then(result => {
+            res.status(200)
+                .json({
+                    status: "success",
+                    message: "berhasil membuat data",
+                    data: result.data.data
+                })
+        }).catch(err => {
+            res.status(400)
+                .json({
+                    status: "gagal",
+                    message: "gagal membuat data",
+                    error: err.message
+                })
+        })
+    },
+    getDetailCustomer: (req, res) => {
+        axios.get(`${orderMicroservice}/partner/detail-partner/${req.params.ptnr_oid}`, {
+            headers: {
+                'authorization': req.get('authorization')
+            }
+        }).then(result => {
+            res.status(200)
+                .json({
+                    status: "success",
+                    message: 'berhasil mengambil data',
+                    data: result.data.data
+                })
+        }).catch(err => {
+            res.status(400)
+                .json({
+                    status: "failed",
+                    message: "gagal mengambil data",
+                    error: err.message
+                })
+        })
     }
 }
 
