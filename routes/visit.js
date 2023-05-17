3402ae918dcfed606f96a6e48738a160c38be1cf
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2023-05-17 11:22:28 +0700

    add endpoint to create schedule visiting

--- routes/visit.js
+++ routes/visit.js
@@ -3,13 +3,17 @@ const router = express.Router()
 const controller = require('../app/Http/Controllers/Controller')
 
 let route = [
-    '/get-visit-schedule',
-    '/get-detail-visit-schedule/:visit_code',
-    '/get-detail-visit/:visited_oid'
+    '/get-visit-schedule', //0
+    '/get-detail-visit-schedule/:visit_code', //1
+    '/get-detail-visit/:visited_oid', //2
+    '/create-schedule-visiting', //3
+    '/people-to-visit', //4
 ]
 
 router.get(route[0], controller.VisitController.getVisitSchedule)
 router.get(route[1], controller.VisitController.getDetailVisitSchedule)
 router.get(route[2], controller.VisitController.getDetailVisit)
+router.post(route[3], controller.VisitController.createScheduleVisit)
+router.post(route[4], controller.VisitController.createPeopleToVisit)
 
 module.exports = router
