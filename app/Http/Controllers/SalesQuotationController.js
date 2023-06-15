7f4ed566888e6d670d855a2b2c7864f17c167f14
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2023-06-15 14:34:10 +0700

    endpoint to get sales quotation history

--- app/Http/Controllers/SalesQuotationController.js
+++ app/Http/Controllers/SalesQuotationController.js
@@ -95,6 +95,29 @@ const SalesQuotationController = {
                     error: err.message
                 })
         })
+    },
+    getSalesQuotation: (req, res) => {
+        axios.get(`${ordermicroservice}/sales-quotation/get-sales-quotation`, {
+            headers: {
+                'authorization': req.get('authorization')
+            }
+        })
+        .then(result => {
+            res.status(200)
+                .json({
+                    status: 'berhasil',
+                    message: 'berhasil mengambil data sales quotation',
+                    sales_quotation_history: result.data.sales_quotation_history
+                })
+        })
+        .catch(err => {
+            res.status(400)
+                .json({
+                    status: 'gagal',
+                    message: 'gagal mengambil data sales quotation',
+                    error: err.message
+                })
+        })
     }
 }
 
