f56a967de9a828f333ff15943ffe285dba1f12bd
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2023-06-16 14:15:22 +0700

    add new API and continue the development for a new API

--- app/Http/Controllers/SalesQuotationController.js
+++ app/Http/Controllers/SalesQuotationController.js
@@ -51,7 +51,7 @@ const SalesQuotationController = {
         })
     },
     getLocationTo: (req, res) => {
-        axios.get(`${ordermicroservice}/sales-quotation/get-location`, {
+        axios.get(`${ordermicroservice}/sales-quotation/get-location-to`, {
             headers: {
                 "authorization": req.get('authorization')
             }
@@ -117,6 +117,64 @@ const SalesQuotationController = {
                 .json({
                     status: 'gagal',
                     message: 'gagal mengambil data sales quotation',
+                    error: err.message
+                })
+        })
+    },
+    getPriceList: (req, res) => {
+        axios.get(`${ordermicroservice}/sales-quotation/get-price-list/partnergroupid/${req.params.partnerGroupId}`, {
+            headers: {
+                "authorization": req.get('authorization')
+            }
+        }).then(result => {
+            res.status(200)
+                .json({
+                    status: 'berhasil',
+                    message: 'berhasil mengambil data list harga',
+                    data: result.data.data
+                })
+        }).catch(err => {
+            res.status(400)
+                .json({
+                    status: 'gagal',
+                    message: 'gagal mengambil data list harga',
+                    error: err.message
+                })
+        })
+    },
+    createSalesQuotation: (req, res) => {
+        axios.post(`${ordermicroservice}/sales-quotation/create-header-sales-quotation`, {
+            sq_ptnr_id_sold: req.body.ptnr_id_sold,
+            sq_pay_type: req.body.pay_type,
+            sq_pay_method: req.body.pay_method,
+            sq_close_date: req.body.close_date,
+            sq_cu_id: req.body.cu_id,
+            sq_trans_rmks: req.body.trans_mrks,
+            sq_cons: req.body.cons,
+            sq_total: req.body.total,
+            sq_terbilang: req.body.terbilang,
+            sq_need_date: req.body.need_date,
+            sq_loc_id: req.body.loc_id,
+            sq_loc_to_id: req.body.loc_to_id,
+            sq_loc_git: req.body.loc_gt_id,
+            sq_is_dropshipper: req.body.is_dropshipper,
+            sq_ship_to: req.body.ship_to
+        }, {
+            headers: {
+                'authorization': req.get('authorization')
+            }
+        }).then(result => {
+            res.status(200)
+                .json({
+                    status: 'berhasil',
+                    message: 'berhasil membuat sales quotation',
+                    data: result.data.data
+                })
+        }).catch(err => {
+            res.status(400)
+                .json({
+                    status: 'gagal',
+                    message: 'gagal membuat sales quotation',
                     error: err.message
                 })
         })
--- routes/sales-quotation.js
+++ routes/sales-quotation.js
@@ -7,7 +7,9 @@ const route = [
     '/location', //1
     '/location-to', //2
     '/location-git', //3
-    '/sales-quotation' //4
+    '/get-sales-quotation', //4
+    '/price-list/partnergroupid/:partnerGroupId', //5
+    '/post-header-sales-quotation' //6
 ]
 
 router.get(route[0], controller.SalesQuotationController.getSite)
@@ -15,5 +17,7 @@ router.get(route[1], controller.SalesQuotationController.getLocation)
 router.get(route[2], controller.SalesQuotationController.getLocationTo)
 router.get(route[3], controller.SalesQuotationController.getLocationGit)
 router.get(route[4], controller.SalesQuotationController.getSalesQuotation)
+router.get(route[5], controller.SalesQuotationController.getPriceList)
+router.post(route[6], controller.SalesQuotationController.createSalesQuotation)
 
 module.exports = router
