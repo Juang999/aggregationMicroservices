0f3d020dea739a3d3956eb4612a98ac5e4ab9f7b
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2023-07-18 15:50:56 +0700

    update route

--- routes/Admin/sales-quotation.js
+++ routes/Admin/sales-quotation.js
@@ -3,7 +3,8 @@ const router = express.Router()
 const route = require('../route')
 const {Admin} = require('../../app/Http/Controllers/Controller')
 
-router.get(route.Admin.SalesQuotation.index, Admin.SalesQuotationController.index)
-router.get(route.Admin.SalesQuotation.invitation, Admin.SalesQuotationController.invitation)
+router.get(route.Admin.feature.SalesQuotation.index, Admin.SalesQuotationController.index)
+router.get(route.Admin.feature.SalesQuotation.invitation, Admin.SalesQuotationController.invitation)
+// router.get(route.Admin.feature.)
 
 module.exports = router
