7b86b80a898f9f8fbecb87ed6ef581b7dfb79577
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2023-08-10 09:47:18 +0700

    udpate

--- config/microservice.js
+++ config/microservice.js
@@ -1,6 +1,7 @@
 const microservices = {
-    ordermicroservice: 'http://192.168.2.7:3000',
-    productknowledgemicroservice: 'http://192.168.2.7:3002'
+    ordermicroservice: 'http://192.168.0.104:3000',
+    productknowledgemicroservice: 'http://192.168.0.104:3002',
+    employeeservice: 'http://192.168.0.104:8000/api'
 }
 
 module.exports = microservices
