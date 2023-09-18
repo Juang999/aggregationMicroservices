a67047c33f4674d330b08db22338f0c4e6fa8fb3
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2023-09-18 09:56:11 +0700

    add filter to get data consigment

--- app/Http/Controllers/Client/PointofSalesController.js
+++ app/Http/Controllers/Client/PointofSalesController.js
@@ -5,8 +5,9 @@ const moment = require('moment')
 class PointofSalesController {
     getLastItemShipped = (req, res) => {
         let defaultEndTimestamp = (req.query.last_date) ? req.query.last_date : ''
+        let salesProgram = (req.query.sales_program) ? req.query.sales_program : ''
 
-        axios.get(`${ordermicroservice}/order-service/client/point-of-sales/${req.params.warehouse_id}/product-consigment?last_date=${defaultEndTimestamp}`, {
+        axios.get(`${ordermicroservice}/order-service/client/point-of-sales/${req.params.warehouse_id}/product-consigment?last_date=${defaultEndTimestamp}&sales_program=${salesProgram}`, {
             headers: {
                 authorization: req.headers['authorization']
             }
