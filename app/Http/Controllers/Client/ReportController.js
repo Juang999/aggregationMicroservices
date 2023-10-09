bdf48364eae674179f72f99c8bb767292c0b0c03
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2023-10-09 11:30:55 +0700

    bomat

--- app/Http/Controllers/Client/ReportController.js
+++ app/Http/Controllers/Client/ReportController.js
@@ -66,6 +66,60 @@ class ReportController {
                 })
         })
     }
+
+    getDetailHistoryDebt = (req, res) => {
+        axios.get(`${ordermicroservice}/order-service/client/report/debt/${req.params.ar_oid}/detail`, {
+            headers: {
+                authorization: req.headers['authorization']
+            }
+        })
+        .then(result => {
+            for (const detailData of result.data.data.data_so) {
+                detailData._links = {
+                    detail_so: `/api/report${links(Client.feature.report.report_detail_shipment, [':so_oid', detailData.arso_so_oid])}`
+                }
+            }
+
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: result.data.data,
+                    error: null
+                })
+        })
+        .catch(err => {
+            res.status(400)
+                .json({
+                    status: 'failed',
+                    data: null,
+                    error: err.response.data.error
+                })
+        })
+    }
+
+    getDetailShipment = (req, res) => {
+        axios.get(`${ordermicroservice}/order-service/client/report/so/${req.params.so_oid}/shipment`, {
+            headers: {
+                authorization: req.headers['authorization']
+            }
+        })
+        .then(result => {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: result.data.data,
+                    error: null
+                })
+        })
+        .catch(err => {
+            res.status(400)
+                .json({
+                    status: 'failed',
+                    data: null,
+                    error: err.response.data.error
+                })
+        })
+    }
 }
 
 module.exports = new ReportController()
